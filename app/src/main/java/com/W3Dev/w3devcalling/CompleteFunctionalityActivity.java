618c9676cf587a22da6d6ddac97f1268be54784f
pk <talk2prajjwal@gmail.com>
2020-12-27 01:17:50 +0530

    in way to make complete Activity for vifdeo rendering from peer connections

--- app/src/main/java/com/W3Dev/w3devcalling/CompleteFunctionalityActivity.java
+++ app/src/main/java/com/W3Dev/w3devcalling/CompleteFunctionalityActivity.java
@@ -4,11 +4,212 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.webrtc.Camera1Enumerator;
+import org.webrtc.Camera2Enumerator;
+import org.webrtc.CameraEnumerator;
+import org.webrtc.DataChannel;
+import org.webrtc.EglBase;
+import org.webrtc.IceCandidate;
+import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnection;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoRenderer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
+import java.util.ArrayList;
+
+import io.socket.client.Socket;
+
 public class CompleteFunctionalityActivity extends AppCompatActivity {
+    public static final String VIDEO_TRACK_ID = "ARDAMSv0";
+    public static final int VIDEO_RESOLUTION_WIDTH = 1280;
+    public static final int VIDEO_RESOLUTION_HEIGHT = 720;
+    public static final int FPS = 30;
+    private static final String TAG = "CompleteActivity";
+    private static final int RC_CALL = 111;
+    private boolean isInitiator;
+    private boolean isChannelReady;
+    private boolean isStarted;
+
+    private SurfaceViewRenderer surface_view1;
+    private SurfaceViewRenderer surface_view2;
+
+
+    private Socket socket;
+    private PeerConnection peerConnection;
+    private EglBase rootEglBase;
+    private PeerConnectionFactory factory;
+    private VideoTrack videoTrackFromCamera;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_functionality);
+        surface_view1 = findViewById(R.id.surface_view1);
+        surface_view2 = findViewById(R.id.surface_view2);
+        start();
+    }
+
+    private void start() {
+        connectToSignallingServer();
+
+        initializeSurfaceViews();
+
+        initializePeerConnectionFactory();
+
+        createVideoTrackFromCameraAndShowIt();
+
+        initializePeerConnections();
+
+        startStreamingVideo();
+
+    }
+
+    private void startStreamingVideo() {
+    }
+
+
+    private void connectToSignallingServer() {
+    }
+
+    private void initializeSurfaceViews() {
+        rootEglBase = EglBase.create();
+        surface_view1.init(rootEglBase.getEglBaseContext(), null);
+        surface_view1.setEnableHardwareScaler(true);
+        surface_view1.setMirror(true);
+
+        surface_view2.init(rootEglBase.getEglBaseContext(), null);
+        surface_view2.setEnableHardwareScaler(true);
+        surface_view2.setMirror(true);
+    }
+
+    private void createVideoTrackFromCameraAndShowIt() {
+        VideoCapturer videoCapturer = createVideoCapturer();
+        VideoSource videoSource = factory.createVideoSource(videoCapturer);
+        videoCapturer.startCapture(VIDEO_RESOLUTION_WIDTH, VIDEO_RESOLUTION_HEIGHT, FPS);
+
+        videoTrackFromCamera = factory.createVideoTrack(VIDEO_TRACK_ID, videoSource);
+        videoTrackFromCamera.setEnabled(true);
+        videoTrackFromCamera.addRenderer(new VideoRenderer(surface_view1));
+
+
+    }
+
+    private void initializePeerConnectionFactory() {
+        PeerConnectionFactory.initializeAndroidGlobals(this, true, true, true);
+        factory = new PeerConnectionFactory(null);
+        factory.setVideoHwAccelerationOptions(rootEglBase.getEglBaseContext(), rootEglBase.getEglBaseContext());
+    }
+
+    private void initializePeerConnections() {
+        peerConnection = createPeerConnection(factory);
+    }
+
+    private PeerConnection createPeerConnection(PeerConnectionFactory factory) {
+        ArrayList<PeerConnection.IceServer> iceServer = new ArrayList<>();
+        iceServer.add(new PeerConnection.IceServer("stun:stun.l.google.com:19302"));
+
+        PeerConnection.RTCConfiguration rtcConfig = new PeerConnection.RTCConfiguration(iceServer);
+        MediaConstraints pcConstraints = new MediaConstraints();
+        PeerConnection.Observer pcObserver = new PeerConnection.Observer() {
+            @Override
+            public void onSignalingChange(PeerConnection.SignalingState signalingState) {
+
+            }
+
+            @Override
+            public void onIceConnectionChange(PeerConnection.IceConnectionState iceConnectionState) {
+
+            }
+
+            @Override
+            public void onIceConnectionReceivingChange(boolean b) {
+
+            }
+
+            @Override
+            public void onIceGatheringChange(PeerConnection.IceGatheringState iceGatheringState) {
+
+            }
+
+            @Override
+            public void onIceCandidate(IceCandidate iceCandidate) {
+
+            }
+
+            @Override
+            public void onIceCandidatesRemoved(IceCandidate[] iceCandidates) {
+
+            }
+
+            @Override
+            public void onAddStream(MediaStream mediaStream) {
+
+            }
+
+            @Override
+            public void onRemoveStream(MediaStream mediaStream) {
+
+            }
+
+            @Override
+            public void onDataChannel(DataChannel dataChannel) {
+
+            }
+
+            @Override
+            public void onRenegotiationNeeded() {
+
+            }
+
+        };
+        return factory.createPeerConnection(rtcConfig, pcConstraints, pcObserver);
+
+
+    }
+
+    private VideoCapturer createVideoCapturer() {
+        VideoCapturer videoCapturer;
+        if (useCamera2()) {
+            videoCapturer = createCameraCapturer(new Camera2Enumerator(this));
+        } else {
+            videoCapturer = createCameraCapturer(new Camera1Enumerator(true));
+        }
+        return videoCapturer;
+    }
+
+    private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private boolean useCamera2() {
+        return Camera2Enumerator.isSupported(this);
     }
 }
