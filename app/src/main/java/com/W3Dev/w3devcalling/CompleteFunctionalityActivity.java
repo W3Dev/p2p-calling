f58372062bbb45327381719e4a70302286bca20a
pk <talk2prajjwal@gmail.com>
2020-12-26 23:55:53 +0530

    in way to make complete Activity for vifdeo rendering from peer connections

---
+++ app/src/main/java/com/W3Dev/w3devcalling/CompleteFunctionalityActivity.java
@@ -0,0 +1,14 @@
+package com.W3Dev.w3devcalling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompleteFunctionalityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_functionality);
+    }
+}
