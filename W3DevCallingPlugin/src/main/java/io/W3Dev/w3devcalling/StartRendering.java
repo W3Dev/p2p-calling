b0776470c5c63499c9516aaf2d8be5c93853bb7a
Pk <talk2prajjwal@gmail.com>
2021-05-13 00:53:59 +0530

    making plugin responsive

--- W3DevCallingPlugin/src/main/java/io/W3Dev/w3devcalling/StartRendering.java
+++ W3DevCallingPlugin/src/main/java/io/W3Dev/w3devcalling/StartRendering.java
@@ -17,47 +17,49 @@ import io.W3Dev.w3devcalling.web_rtc.AppRTCAudioManager;
 
 public class StartRendering {
 
-
-    // MediaConstraints audioConstraints;
-    public static final String VIDEO_TRACK_ID = "ARDAMSv0";
+    //    MediaConstraints mediaConstraints;
     public static final int VIDEO_RESOLUTION_WIDTH = 1280;
     public static final int VIDEO_RESOLUTION_HEIGHT = 720;
     public static final int FPS = 24;
     AudioSource audioSource;
     AudioTrack localAudioTrack;
+//    private AppRTCAudioManager audioManager;
+//    VideoCapturer videoCapturer;
+//PeerConnectionFactory factory,
+//    PeerConnection peerConnectionFactory
+//    private VideoTrack videoTrackFromCamera;
+
     Context context;
     SetupViews setupViews = new SetupViews();
-    private AppRTCAudioManager audioManager;
-    private VideoTrack videoTrackFromCamera;
-    private PeerConnectionFactory factory;
 
-    public void createVideoTrackFromCamandShow(MediaConstraints audioConstraints) {
-        audioConstraints = new MediaConstraints();
-        VideoCapturer videoCapturer = setupViews.createVideoCapturer();
-        VideoSource videoSource = factory.createVideoSource(videoCapturer);
+
+    public void createVideoTrackFromCamandShow(MediaConstraints audioConstraints,
+                                               PeerConnectionFactory factory,
+                                               VideoCapturer videoCapturer,
+                                               VideoSource videoSource,
+                                               VideoTrack videoTrackFromCamera,
+                                               AppRTCAudioManager audioManager) {
+
+//        audioConstraints = new MediaConstraints();
+        videoCapturer = setupViews.createVideoCapturer();
+        videoSource = factory.createVideoSource(videoCapturer);
         videoCapturer.startCapture(VIDEO_RESOLUTION_WIDTH, VIDEO_RESOLUTION_HEIGHT, FPS);
 
-        videoTrackFromCamera = factory.createVideoTrack(VIDEO_TRACK_ID, videoSource);
+        videoTrackFromCamera = factory.createVideoTrack(getTrackID(), videoSource);
         videoTrackFromCamera.setEnabled(true);
         //videoTrackFromCamera.addRenderer(new VideoRenderer(surface_view1));
 
         audioManager = AppRTCAudioManager.create(context);
         audioManager.start(this::onAudioManagerDevicesChanged);
 
-
         audioSource = factory.createAudioSource(audioConstraints);
         localAudioTrack = factory.createAudioTrack("101", audioSource);
     }
 
 
-    public void onAudioManagerDevicesChanged(
-            final AppRTCAudioManager.AudioDevice device,
-            final Set<AppRTCAudioManager.AudioDevice> availableDevices) {
-        // TODO(henrika): add callback handler.
-    }
-
-
-    public void startStreamingVideo(PeerConnection peerConnection) {
+    public void startStreamingVideo(PeerConnection peerConnection,
+                                    PeerConnectionFactory factory,
+                                    VideoTrack videoTrackFromCamera) {
         MediaStream mediaStream = factory.createLocalMediaStream("ARDAMS");
         mediaStream.addTrack(videoTrackFromCamera);
         mediaStream.addTrack(localAudioTrack);
@@ -65,5 +67,15 @@ public class StartRendering {
         //   sendMessage("got user media");
     }
 
+    private String getTrackID() {
+        final String VIDEO_TRACK_ID = "ARDAMSv0";
+        return VIDEO_TRACK_ID;
+    }
+
+    private void onAudioManagerDevicesChanged(
+            final AppRTCAudioManager.AudioDevice device,
+            final Set<AppRTCAudioManager.AudioDevice> availableDevices) {
+        // TODO(henrika): add callback handler.
+    }
 
 }
